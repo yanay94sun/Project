c352d37fdb34f5a2689468f2a3c8aa0e0c43d98a
Asaf Stern <t-asafstern@microsoft.com>
2024-03-11 22:14:46 +0200

    -working dtc

--- Baseline/baseline.py
+++ Baseline/baseline.py
@@ -6,26 +6,44 @@ from sklearn.model_selection import train_test_split # Import train_test_split f
 from sklearn import metrics #Import scikit-learn metrics module for accuracy calculation
 
 
-def evaluate_model(classifier: DecisionTreeClassifier, X_test: pd.DataFrame, y_test: pd.DataFrame):
+def evaluate_and_visualize_model(classifier: DecisionTreeClassifier, X_test: pd.DataFrame, y_test: pd.DataFrame):
     y_pred = classifier.predict(X_test)
 
     print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
+    import matplotlib.pyplot as plt
+    from sklearn import tree
+
+    # Assuming clf is your trained DecisionTreeClassifier object
+    plt.figure(figsize=(12, 8))
+    tree.plot_tree(classifier, filled=True, feature_names=X_test.columns)
+    plt.show()
+
+
+
+def filter_relevant_cols_for_model(df: pd.DataFrame):
+    feature_cols = df.columns[:-1]  # removing the label column
+    remove_keys = ["date", "day", "year", "month", "_id"]
+    feature_cols = list(filter(lambda col: not any([key in col for key in remove_keys]), feature_cols))
+    return feature_cols, [df.columns[-1]]
 
 
 def create_model(df: pd.DataFrame):
-    feature_cols = df.columns[:-1]
-    label_col = [df.columns[-1]]
+    feature_cols, label_cols = filter_relevant_cols_for_model(df)
+    # feature_cols = df.columns[:-1]
+    # label_col = [df.columns[-1]]
+    df = df.apply(pd.to_numeric, errors='coerce')
     X = df[feature_cols]
-    y = df[label_col[0]]
+    y = df[label_cols]
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3,
                                                         random_state=1)  # 70% training and 30% test
 
-    clf = DecisionTreeClassifier()
+    clf = DecisionTreeClassifier(max_depth=None, min_samples_split=2, min_samples_leaf=1, max_features=None)
 
     # Train Decision Tree Classifier
     clf = clf.fit(X_train, y_train)
-    evaluate_model(clf, X_test, y_test)
+    evaluate_and_visualize_model(clf, X_test, y_test)
+    return clf
 
 
 if __name__ == '__main__':
@@ -40,5 +58,6 @@ if __name__ == '__main__':
         window_size_y=1,
         target_label_column_name="disrupt")
     time_series_df = time_series_df.dropna()
-    create_model(time_series_df)
+    dtc = create_model(time_series_df)
+
 
