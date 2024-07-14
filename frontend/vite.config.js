b14d944112530808b7393de14d1fedf1eeaf2624
Yanay <yanayshem.com>
2024-07-14 23:37:14 +0300

    a

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -11,6 +10,11 @@ export default defineConfig({
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: 'index.html', // Ensure this points to your index.html file
     },
   },
 });
